26f6b80a6780cf6ce801bb1c73c9ca85030570f9
Habiba <habebaesam13@gmail.com>
2025-08-04 23:20:15 +0300

    nurse and service tables added

---
+++ app/Http/Controllers/RequestServiceController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RequestService;
+use Illuminate\Http\Request;
+
+class RequestServiceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(RequestService $requestService)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(RequestService $requestService)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, RequestService $requestService)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(RequestService $requestService)
+    {
+        //
+    }
+}
